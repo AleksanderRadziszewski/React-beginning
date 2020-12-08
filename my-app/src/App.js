bfc820de498170dddd731a59de613db57989f1f5
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-12-08 13:02:24 +0100

    tweets

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,10 @@ function App() {
 
     return(
     <div className= "app">
-        <h1>Hello React</h1>
-        <Tweet />
+        <Tweet name="Dev ED" message="This is a random tweet" numbers_of_likes="70"/>
+        <Tweet name="John Snow" message="I am the true king" numbers_of_likes="60"/>
+        <Tweet name="Traversy Media" message="700k my dudes" numbers_of_likes="40"/>
+        <Tweet name="Mosh" message="My course is available" numbers_of_likes="30"/>
     </div>
 
     );

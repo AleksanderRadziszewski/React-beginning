9c73f1672964b03476aa8b2bcbe0d9110da23f32
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-12-10 12:51:01 +0100

    react_managment

---
+++ reactmanagement/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MovieList from './MovieList'
+import Nav from './Nav'
+import './App.css';
+import {MovieProvider} from './MovieContext'
+
+function App() {
+
+
+  return (
+    <MovieProvider>
+    <div className="App">
+      <Nav />
+      <MovieList />
+    </div>
+    </MovieProvider>
+  );
+}
+
+export default App;

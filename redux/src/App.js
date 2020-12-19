2980c83d1ad85c391c79927dfaf3e769cdb44a3b
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-12-19 22:29:34 +0100

    redux counter and isLogged

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
+
 function App() {
+  const counter = useSelector( state => state.counter);
+  const isLogged = useSelector( state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick = {() => dispatch(increment(2))}>+</button>
+      <button onClick = {() => dispatch(decrement(2))}>-</button>
+      {isLogged ? <h3>Valuable infromation I shouldn't see</h3> : '' }
     </div>
   );
 }
--- redux/src/index.js
+++ redux/src/index.js
@@ -1,40 +1,18 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
+import allReducers from './reducers';
+import {Provider} from 'react-redux';
 
-//ACTION increment
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return {
-    type:'DECREMENT'
-  }
-}
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-//REDUCER
-const counter = (state = 0, action) => {
-        switch(action.type){
-          case "INCREMENT":
-            return state + 1;
-          case "DECREMENT":
-            return state - 1;
-        }
-}
-
-let store = createStore(counter);
-
-// Display in console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-ReactDOM.render(document.getElementById('root'));
+ReactDOM.render(
+    <Provider store = {store}>
+        <App/>,
+    </Provider>,
+document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
